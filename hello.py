cf9680038cf662e638dc431f3a91c1968e0fc48a
davidcbhunter <hunter.davidcb@gmail.com>
2023-05-15 15:30:20 +0900

    First commit
    This is the first commit for the afterschool programming group.

---
+++ hello.py
@@ -0,0 +1,16 @@
+message = "hello world"
+message2 = 12
+message3 = 78.8989891124537845752876831537888
+message4 = "Mr. Hunter"
+
+print(message)
+
+print(type(message))
+print(type(message2))
+print(type(message3))
+#print(type(message4))
+v = message4 == "pizza"
+if  v:
+    print("delicious")
+
+print(v)
