0e631a03dd73b7a4fc1883db54ce25ecfe602872
davidcbhunter <hunter.davidcb@gmail.com>
2023-05-29 15:47:17 +0900

    Add files via upload

---
+++ greeter.py
@@ -0,0 +1,21 @@
+greet_student()
+
+
+def greet_student():
+    student_name = input("Enter your name \n")
+
+    print("Hi, " + student_name)
+
+#num_students = input("How many students are here?\n")
+
+for x in range(4):
+    greet_student()
+
+
+greet_student()
+
+
+
+
+
+
