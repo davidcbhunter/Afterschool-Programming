22a33cbb552486ba6ef4646edfec82bb4f49d032
davidcbhunter <hunter.davidcb@gmail.com>
2023-07-07 14:55:17 +0900

    Add files via upload

---
+++ function types.py
@@ -0,0 +1,44 @@
+def greet():
+    print("Hi, Mei and Yuna!")
+
+def greet_two(name_one,name_two):
+    print("Hi, " + name_one + " and " + name_two)
+
+
+def greet_three():
+    return "Hi, Yuna and Mei"
+
+def greet_four(name_one, name_two):
+    return "Hi, " + name_one + " and " + name_two
+
+message = greet_four("Momoka","Mio")
+
+print(message)
+
+#add, no return
+def add(num_one,num_two):
+    print(num_one+num_two)
+
+#add, return
+def add_two(num_one,num_two):
+    return num_one+num_two
+
+answer = add_two(1234134,13451234)
+
+
+#functions with no variables, and no return
+# ????
+
+#functions with variables, and no return
+print("Hello, everyone")
+
+
+#functions with no variables, return
+message = message.upper()
+message = message.islower()
+
+#functions with variables, and return
+memory_location = id(message)
+v = message.count("MO")
+
+
