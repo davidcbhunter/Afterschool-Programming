7e5c66365dfd03660bfbafc9d76f264c5e6d8d27
davidcbhunter <hunter.davidcb@gmail.com>
2023-07-10 15:41:33 +0900

    Add files via upload

---
+++ 7-10_functions.py
@@ -0,0 +1,22 @@
+message_one ="Please enter your first name\n"
+message_two ="Please enter your last name\n"
+
+#message_three = 3.141592653758979
+#mess = 7
+
+#a = message_three + mess
+#print(a)
+
+first_name = input(message_one)
+
+last_name = input(message_two)
+
+first_name = first_name.capitalize()
+
+last_name = last_name.capitalize()
+
+print(first_name)
+print(last_name)
+
+ans = print(first_name)
+print(ans)
